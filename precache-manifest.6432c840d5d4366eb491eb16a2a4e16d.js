ff2929341dc09567fb99f04bf0fe52fdba4b38d5
Ajitesh Chhatani <ajiteshchhatani@gmail.com>
2020-10-04 12:46:37 +0530

    Updates

---
+++ precache-manifest.6432c840d5d4366eb491eb16a2a4e16d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0196477a32a764caaab51de785943d80",
+    "url": "/fof-fpl/index.html"
+  },
+  {
+    "revision": "6ddaa5a1648a0a98d272",
+    "url": "/fof-fpl/static/css/main.90c1947e.chunk.css"
+  },
+  {
+    "revision": "6650c119cfc0881c8f97",
+    "url": "/fof-fpl/static/js/2.15c98e6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fof-fpl/static/js/2.15c98e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddaa5a1648a0a98d272",
+    "url": "/fof-fpl/static/js/main.4cdd7deb.chunk.js"
+  },
+  {
+    "revision": "54a2a6941a28b19fe846",
+    "url": "/fof-fpl/static/js/runtime-main.f838ceea.js"
+  }
+]);
